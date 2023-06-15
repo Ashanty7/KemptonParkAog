1da13265c021f5d050fc5c772fe18244091fb2f1
Shantell Nkwana <shantell.nkwana@absa.africa>
2023-06-15 11:35:15 +0200

    Login Screen - part 1

---
+++ app/src/main/java/com/example/kemptonparkaog/LoginFragment.kt
@@ -0,0 +1,12 @@
+package com.example.kemptonparkaog
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+class LoginFragment : Fragment(R.layout.login_fragment) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
