edbc6ef563885f2de7472b0afd979f855756d988
Shantell Nkwana <shantell.nkwana@absa.africa>
2023-07-05 17:04:48 +0200

    Updated exposed dropdown list

--- app/src/main/java/com/example/kemptonparkaog/UserRegistrationDetailsFragment.kt
+++ app/src/main/java/com/example/kemptonparkaog/UserRegistrationDetailsFragment.kt
@@ -26,19 +26,15 @@ class UserRegistrationDetailsFragment : BaseFragment(R.layout.user_registration_
     }
 
     private fun setupAdapterForGender() {
-        val genderAdapter = ArrayAdapter.createFromResource(
-            requireContext(), R.array.gender, R.layout.testing_dropdown
-        )
-        genderAdapter.setDropDownViewResource(androidx.appcompat.R.layout.support_simple_spinner_dropdown_item)
-        binding.genderSpinner.adapter = genderAdapter
+        val feelings = resources.getStringArray(R.array.gender)
+        val arrayAdapter = ArrayAdapter(requireContext(), R.layout.testing_dropdown, feelings)
+        binding.genderAutoCompleteTextView.setAdapter(arrayAdapter)
     }
 
     private fun setupAdapterForMaritalStatus() {
-        val maritalStatusAdapter = ArrayAdapter.createFromResource(
-            requireContext(), R.array.marital_status, R.layout.testing_dropdown
-        )
-        maritalStatusAdapter.setDropDownViewResource(androidx.appcompat.R.layout.support_simple_spinner_dropdown_item)
-        binding.maritalStatusSpinner.adapter = maritalStatusAdapter
+        val feelings = resources.getStringArray(R.array.marital_status)
+        val arrayAdapter = ArrayAdapter(requireContext(), R.layout.testing_dropdown, feelings)
+        binding.maritalStatusAutoCompleteTextView.setAdapter(arrayAdapter)
     }
 
     private fun setupOnclickListeners() {
