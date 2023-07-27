6a87097047718c31794a8a1686e62dc320606f10
Shantell Nkwana <shantell.nkwana@absa.africa>
2023-07-27 18:34:58 +0200

    Main Fragment

---
+++ app/src/main/java/com/example/kemptonparkaog/view/MainFragment.kt
@@ -0,0 +1,12 @@
+package com.example.kemptonparkaog.view
+
+import android.os.Bundle
+import android.view.View
+import com.example.kemptonparkaog.R
+
+class MainFragment : BaseFragment(R.layout.main_fragment) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
